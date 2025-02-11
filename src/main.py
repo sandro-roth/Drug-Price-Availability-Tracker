7c03ebe416e8b97f59194addcae0c3608584976f
sandro-roth <sandro.roth@hotmail.de>
2025-02-11 19:28:02 +0100

    minor changes in logger settings

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
 # Main script to execute the project
+import logging
+
+from dotenv import find_dotenv, load_dotenv
+
+from utils import setup_logging
+
+# Initial setup find .env file, setup paths for config and logs directory
+env_setup = find_dotenv()
+load_dotenv()
+
+config_dir = "../config"
+log_dir = "../logs"
+
+
+if __name__ == '__main__':
+    pass
+    # setup_logging(config_dir, log_dir)
+    # Main logger
+    # logger = logging.getLogger(__name__)

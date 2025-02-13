d3c1af17e4f104dcccb09244998c06e0d5f80f24
sandro-roth <sandro.roth@hotmail.de>
2025-02-13 10:39:47 +0100

    initialize testing for Scrapper class

---
+++ tests/scraping/test_scraper.py
@@ -0,0 +1,40 @@
+# Unit tests web scraping functions
+import unittest
+
+from src.scraping.scraper import Scraper
+from src.scraping.config import headers, zurose_url, zurose_str
+
+
+class TestScraper(unittest.TestCase):
+
+    def test_instance_no_parameter(self):
+        # Check for type error for missing parameter
+        with self.assertRaises(TypeError):
+            Scraper()
+
+    def test_instance_with_parameter(self):
+        scraper = Scraper(header=headers)
+
+# ----- Testing the first part of the Scrapper Class to see if the information from the Zurose webpage is downloaded
+    def test_response_status_code_zurose(self):
+        scraper = Scraper(header=headers)
+        self.assertTrue(scraper.check_response(url=zurose_url) == 200)
+
+    def test_link_to_file_not_found_zurose(self):
+        scraper = Scraper(header=headers)
+        with self.assertRaises(ValueError):
+            scraper.save_file(url=zurose_url, web_button='this is the wrong string')
+
+    def test_link_to_file_correct_zurose(self):
+        scraper = Scraper(header=headers)
+        link = scraper.save_file(url=zurose_url, web_button=zurose_str)
+        print(link)
+        self.assertTrue(link)
+
+
+    # check if file is saved in data/raw
+
+
+
+if __name__ == '__main__':
+    unittest.main()

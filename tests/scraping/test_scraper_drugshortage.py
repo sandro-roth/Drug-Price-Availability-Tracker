7b70e88a22cb5c7f3478523b8a399f3a07431ea0
sandro-roth <sandro.roth@hotmail.de>
2025-02-15 21:38:50 +0100

    include setUp method for testing

--- tests/scraping/test_scraper_drugshortage.py
+++ tests/scraping/test_scraper_drugshortage.py
@@ -6,23 +6,18 @@ from src.scraping.config import headers, drug_url
 class TestScrapper(unittest.TestCase):
 
     # Testing the first part of the Scrapper Class
-    def test_instance_no_parameter(self):
-        # Check for type error for missing parameter
-        with self.assertRaises(TypeError):
-            Scraper()
+    def setUp(self):
+        self.scraper = Scraper(header=headers)
 
     def test_instance_with_parameter(self):
-        scraper = Scraper(header=headers)
-        self.assertIsInstance(scraper, Scraper)
+        self.assertIsInstance(self.scraper, Scraper)
 
     def test_loading_settings(self):
-        scraper = Scraper(header=headers)
-        parameter = scraper.load_yaml()
+        parameter = self.scraper.load_yaml()
         self.assertIsInstance(parameter, dict)
 
     def test_conn_to_web_and_druglist(self):
-        scraper = Scraper(header=headers)
-        list_of_drugs = scraper.connect_shortage(drug_url)
+        list_of_drugs = self.scraper.connect_shortage(drug_url)
         self.assertTrue(isinstance(list_of_drugs, list))
 
 

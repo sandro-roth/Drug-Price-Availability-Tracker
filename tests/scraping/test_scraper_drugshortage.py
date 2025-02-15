4114a5178a7b4fe29527657393b799dbb156ed65
sandro-roth <sandro.roth@hotmail.de>
2025-02-15 21:08:22 +0100

    initial testing setup for drugshortage scraping

--- tests/scraping/test_scraper_drugshortage.py
+++ tests/scraping/test_scraper_drugshortage.py
@@ -1,7 +1,7 @@
 import unittest
 
 from src.scraping.scraper import Scraper
-from src.scraping.config import headers
+from src.scraping.config import headers, drug_url
 
 class TestScrapper(unittest.TestCase):
 
@@ -14,6 +14,16 @@ class TestScrapper(unittest.TestCase):
     def test_instance_with_parameter(self):
         scraper = Scraper(header=headers)
         self.assertIsInstance(scraper, Scraper)
+
+    def test_loading_settings(self):
+        scraper = Scraper(header=headers)
+        parameter = scraper.load_yaml()
+        self.assertIsInstance(parameter, dict)
+
+    def test_conn_to_web_and_druglist(self):
+        scraper = Scraper(header=headers)
+        list_of_drugs = scraper.connect_shortage(drug_url)
+        self.assertTrue(isinstance(list_of_drugs, list))
 
 
 if __name__ == '__main__':

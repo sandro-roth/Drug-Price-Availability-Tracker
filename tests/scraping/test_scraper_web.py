d4e7214851a61620ee33b3d4c8dfad2434bcefe3
sandro-roth <sandro.roth@hotmail.de>
2025-02-15 19:01:09 +0100

    minor changes: excluded tests which will be tested in other script

--- tests/scraping/test_scraper_web.py
+++ tests/scraping/test_scraper_web.py
@@ -1,6 +1,4 @@
-# Unit tests web scraping functions
 import unittest
-from urllib.error import HTTPError
 
 from src.scraping.scraper import Scraper
 from src.scraping.config import headers, zurose_url, zurose_str
@@ -8,15 +6,7 @@ from src.scraping.config import headers, zurose_url, zurose_str
 
 class TestScraper(unittest.TestCase):
 
-    def test_instance_no_parameter(self):
-        # Check for type error for missing parameter
-        with self.assertRaises(TypeError):
-            Scraper()
-
-    def test_instance_with_parameter(self):
-        scraper = Scraper(header=headers)
-
-# ----- Testing the second part of the Scrapper Class to see if the information from the Zurose webpage is downloaded
+    # Testing the second part of the Scrapper Class to see if the information from the Zurose webpage is downloaded
     def test_response_status_code_zurose(self):
         scraper = Scraper(header=headers)
         self.assertTrue(scraper.check_response(url=zurose_url) == 200)

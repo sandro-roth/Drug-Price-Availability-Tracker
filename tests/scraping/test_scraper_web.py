ec1ba8e9da093247732b86347bc76874a0ff460d
sandro-roth <sandro.roth@hotmail.de>
2025-02-15 21:42:37 +0100

    include setUp method for testing

--- tests/scraping/test_scraper_web.py
+++ tests/scraping/test_scraper_web.py
@@ -7,18 +7,18 @@ from src.scraping.config import headers, zurose_url, zurose_str
 class TestScraper(unittest.TestCase):
 
     # Testing the second part of the Scrapper Class to see if the information from the Zurose webpage is downloaded
+    def setUp(self):
+        self.scraper = Scraper(header=headers)
+
     def test_response_status_code_zurose(self):
-        scraper = Scraper(header=headers)
-        self.assertTrue(scraper.check_response(url=zurose_url) == 200)
+        self.assertTrue(self.scraper.check_response(url=zurose_url) == 200)
 
     def test_link_to_file_not_found_zurose(self):
-        scraper = Scraper(header=headers)
         with self.assertRaises(ValueError):
-            scraper.save_file(url=zurose_url, web_button='this is the wrong string')
+            self.scraper.save_file(url=zurose_url, web_button='this is the wrong string')
 
     def test_link_to_file_correct_zurose(self):
-        scraper = Scraper(header=headers)
-        file = scraper.save_file(url=zurose_url, web_button=zurose_str)
+        file = self.scraper.save_file(url=zurose_url, web_button=zurose_str)
         self.assertIsNone(file.raise_for_status())
 
 

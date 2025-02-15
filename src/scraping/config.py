25ee4d348d04cfaaaf8a880d51c2c20fd1d6c6a4
sandro-roth <sandro.roth@hotmail.de>
2025-02-15 20:56:02 +0100

    added url to drugshortages

--- src/scraping/config.py
+++ src/scraping/config.py
@@ -1,4 +1,5 @@
 # Scraper settings (URLs, headers, etc.)
-headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 OPR/116.0.0.0"}
-zurose_url="https://www.zurrose.ch/de/aerzte/nota/weshalb"
-zurose_str='Liste nicht lieferbare Lagerartikel abrufen'
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 OPR/116.0.0.0"}
+zurose_url = "https://www.zurrose.ch/de/aerzte/nota/weshalb"
+zurose_str = 'Liste nicht lieferbare Lagerartikel abrufen'
+drug_url = 'https://www.drugshortage.ch'
